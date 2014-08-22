619973c67f19e2082ecdb123e3cf6150c8fa7d47
martialdidit <martial.puygrenier@gmail.com>
2014-08-22 18:04:08 +0200

    add demo version

--- app/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-#------------------------[ INSERT YOUR INFORMATION FORM YOUR DATABASE ]------------------------------
-
-$host = "localhost";
-$dbname = "ipsum";
-$user = "user";
-$password = "password";
---
+++ app/controller/ChannelCtrl.js
@@ -0,0 +1,46 @@
+app.controller('ChannelCtrl', ['$scope', 'googleService', 'shareProperty', '$http',
+    function($scope, googleService, shareProperty, $http) {
+
+        $scope.user = {};
+        $scope.network = {};
+        $scope.error = {};
+
+        $scope.login = function () {
+            googleService.handleAuthResult().then(function (data) {
+                $scope.user.connect = true;
+                $scope.user.name = data.fullName;
+                $scope.user.YouTube = "http://youtube.com/channel/" + data.id;
+                $scope.user.email = data.email;
+                $scope.user.analytics = data.analytics;
+                $scope.user.view = data.view;
+                $scope.user.subscriber = data.subs;
+                $scope.step.one = true;   
+            }, function (error) {
+                console.log('Failed: ' + error)
+            });
+        };
+
+        var reciepInfosNetwork = shareProperty.getNetwork();
+        reciepInfosNetwork.then(function(result) {  
+            $scope.user.contract = result.data.contract;
+            $scope.network.name = result.data.name;
+            $scope.network.analytics = result.data.analytics;
+            $scope.network.email = result.data.email;
+        });
+
+        $scope.valideAnalytics = function () {
+            if($scope.network.analytics <= $scope.user.analytics)
+                return true;
+            return false;
+        };  
+        
+        $scope.$watch(function () {
+            $scope.user.contract = shareProperty.getContract().contract;
+        });
+            
+        $scope.saveUser = function () {
+            $scope.user.contract = true;
+        };
+
+    }
+]);
